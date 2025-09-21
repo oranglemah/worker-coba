3ed0a82e06e1ad6111ab7f18a365adfc81fc7a02
oranglemah <zulfadliarsenio@gmail.com>
2025-09-21 15:38:19 +0700

    Update link.js

--- web/js/link.js
+++ web/js/link.js
@@ -11,7 +11,7 @@ let filteredProxyList = []
 let selectedProxy = null
 const defaultProxyUrl = "https://raw.githubusercontent.com/AFRcloud/ProxyList/refs/heads/main/ProxyList.txt"
 
-const serverDomains = ["sirtu.oranglemah.web.id"]
+const serverDomains = ["sirtu.oranglemah.my.id"]
 let selectedServerDomain = "sirtu.oranglemah.my.id"
 const defaultUUID = "8febb7c9-a664-4b16-bbc5-563b099a4860"
 const itemsPerPage = 10
